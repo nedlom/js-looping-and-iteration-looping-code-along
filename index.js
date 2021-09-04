625171fb48d983d90338a1d2f09eabf28659920b
Nick <nelom3@gmail.com>
2021-09-04 13:04:58 -0700

    commit tree 02e1fdfec9a0a7b409b3d24c2b3ba29874d3bfd4

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(names, eventName) {
+  const customMessages = [];
+  for (let i = 0; i < names.length; i++) {
+    customMessages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+  }
+
+  return customMessages;
+}
+
+function countDown(positiveInt) {
+  while(positiveInt >= 0) {
+    console.log(positiveInt)
+    positiveInt--
+  };
+
+}
